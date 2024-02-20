1d446f8b2f541387adc6839ea3bd31978f60033f
Elis Kandel <alishkandel2062@gmail.com>
2024-02-20 17:06:41 +0545

    y

---
+++ d2.py
@@ -0,0 +1,18 @@
+'''a=[1,2,3,4,5,6,7,8,9]
+iseven=(a[0]%2)==0
+b=len(a)
+if iseven is True:
+    print("Even")
+else:
+    print("Odd")
+'''
+#user input 
+# user_input=input("Enter your name")
+# user_age=int(input("ENTER YOUR AGE"))
+# print(f'{user_input} is {user_age}')
+
+user_age=int(input("ENTER YOUR AGE"))
+if user_age>=18:
+    print("You can go")
+else:
+    print("You cannot go")
