f62884cddef19a275747f322e018bf2df7be77e5
Elis Kandel <alishkandel2062@gmail.com>
2024-02-21 17:17:28 +0545

    Update func.py

--- func.py
+++ func.py
@@ -1,3 +1,18 @@
-def sum(a,b):
-    print(a+b)
-sum(10,20)
+# def sum(a,b):
+#     print(a+b)
+# sum(10,20)
+
+a=[1,2,3,4,5,6,7,8,9,10,11,12,3,14]
+def count(a):
+    dict={
+    "odd":0,
+    "even":0
+}
+    for i in range(1,len(a)):
+        if a[i]%2==0:
+            dict["odd"]+=1
+        else:
+            dict["even"]+=1
+    
+    print(dict)
+count(a)
